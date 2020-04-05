56427c40c61c290a71606b4f288dc4f33e496ad2
mishkigami <mihail.kodolov@gmail.com>
2020-04-05 20:56:45 +0400

    validates name length to 30, combined validates email

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,7 @@
 class Admin < ApplicationRecord
   validates :name, :email, :role, :password_digest, presence: true
-  validates :name, length: { maximum: 10 }
-  validates :email, uniqueness: true
-  validates :email, email: true
+  validates :name, length: { maximum: 30 }
+  validates :email, uniqueness: true, email: true
   validates :role, inclusion: { in: %w(Admin Editor),
                    message: "%{value} is not Admin or Editor" }
   validates :password_digest, length: { minimum: 6 }
