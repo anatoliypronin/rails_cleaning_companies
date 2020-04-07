45325599623832398f2889323ea4e423ba9e7124
mishkigami <mihail.kodolov@gmail.com>
2020-04-07 17:49:42 +0400

    change validates presence: delete website,phone_number

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ApplicationRecord
   validates :name, :email, :password_digest, :requisites, :description,
-  :rating, :website, :phone_number, presence: true
+  :rating, presence: true
   validates :name, length: { maximum: 30 }
   validates :email, uniqueness: true, email: true
   validates :phone_number, phone: true
