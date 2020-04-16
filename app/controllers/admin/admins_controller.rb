1ecdea60c4481dba1804e00bef33cbb32aaa534c
mishkigami <mihail.kodolov@gmail.com>
2020-04-16 04:47:50 +0400

    add links pages,add method new, add view new.html,add test new admins page

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -2,4 +2,8 @@ class Admin::AdminsController < Admin::ApplicationController
     def index
         @admins = Admin.all
     end
+
+    def new
+        @admin = Admin.new
+    end
 end
--- test/controllers/admin/admins_controller_test.rb
+++ test/controllers/admin/admins_controller_test.rb
@@ -3,8 +3,13 @@ require 'test_helper'
 class Admin::AdminsControllerTest < ActionDispatch::IntegrationTest
   test 'should get index admins page' do
     create :admin
-    
+
     get admin_admins_path
+    assert_response :success
+  end
+
+  test 'should get new admins page' do
+    get new_admin_admin_path
     assert_response :success
   end
 end
