32f826329e6597fd78432b7f15fcbab2088ce774
mishkigami <mihail.kodolov@gmail.com>
2020-04-16 09:24:29 +0400

    add view show, add test show, create

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -6,4 +6,23 @@ class Admin::AdminsController < Admin::ApplicationController
     def new
         @admin = Admin.new
     end
+
+    def create
+        @admin = Admin.new(admins_attrs)
+        if @admin.save
+            redirect_to action: :index
+        else
+            render action: :new
+        end    
+    end
+
+    def show
+        @admin = Admin.find(params[:id])
+    end
+
+    private
+
+    def admins_attrs
+        params.require(:admin).permit(:name, :email, :password_digest, :role)
+    end
 end
