a4a9b2756a293181175a4d4b6a05dbad98934888
Nailok <lexitor34@gmail.com>
2020-04-18 00:10:50 +0300

    Add crud + tests

--- app/controllers/admin/clients_controller.rb
+++ app/controllers/admin/clients_controller.rb
@@ -4,6 +4,47 @@ class Admin::ClientsController < Admin::ApplicationController
   end
 
   def new 
-    @clients = Client.new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new(client_attrs)
+
+    if @client.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  def show
+    @client = Client.find(params[:id]) 
+  end
+
+  def edit
+    @client = Client.find(params[:id]) 
+  end
+
+  def update
+    @client = Client.find(params[:id]) 
+
+    if @client.update(client_attrs)
+      redirect_to action: :index
+    else
+      render action: :edit
+    end
+  end
+  
+  def destroy
+    client = Client.find(params[:id])
+    client.destroy
+
+    redirect_to action: :index
+  end
+
+  private
+
+  def client_attrs
+    params.require(:client).permit(:name, :surname, :email, :phone_number, :password_digest)
   end
 end
--- test/controllers/admin/clients_controller_test.rb
+++ test/controllers/admin/clients_controller_test.rb
@@ -9,7 +9,61 @@ class Admin::ClientControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get new client page' do
-    get new_admin_client
+    get new_admin_client_path
     assert_response :success
+  end
+  
+  test 'should create client' do
+    client_attrs = attributes_for :client
+    post admin_clients_path, params: { client: client_attrs }
+    assert_response :redirect 
+  
+    client = Client.last 
+    assert_equal client_attrs[:email], client.email
+  end
+
+  test 'should not create client' do
+    client_attrs = attributes_for :client, email: nil
+
+    post admin_clients_path, params: { client: client_attrs }
+    assert_response :success
+  
+    client = Client.find_by(email: client_attrs[:email])
+    assert_nil client
+  end
+
+  test 'should get show client page' do
+    client = create :client 
+    get admin_client_path(client)
+    assert_response :success
+  end
+
+  test 'should get edit client page' do
+    client = create :client
+
+    get edit_admin_client_path(client.id)
+    assert_response :success
+  end
+
+  test 'should put update client' do
+    client = create :client
+
+    attrs = {}
+    attrs[:surname] = generate :surname
+
+    put admin_client_path(client.id), params: { client: attrs }
+    assert_response :redirect
+
+    client.reload
+    assert_equal attrs[:surname], client.surname
+  end
+
+  test 'should destroy client' do
+    client = create :client
+
+    delete admin_client_path(client.id)
+    assert_response :redirect
+
+    assert_not Client.exists?(client.id)
   end
 end
