dddf47c7006b6b79f35c05888373418403d2da7e
Nailok <lexitor34@gmail.com>
2020-04-27 19:08:43 +0300

    Add crud methods

---
+++ app/controllers/admin/service_controller.rb
@@ -0,0 +1,49 @@
+class Admin::ServiceController < Admin::ApplicationController
+  def index
+    @services = Service.all
+  end
+
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_attrs)
+    if @service.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+    end
+
+  def show
+    @service = Service.find(params[:id])
+  end
+
+  def edit
+    @service = Service.find(params[:id])
+  end
+
+  def update
+    @service = Service.find(params[:id])
+    if @service.update(service_attrs)
+      redirect_to action: :index
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    service = Service.find(params[:id])
+    service.destroy
+
+    redirect_to action: :index
+  end
+
+  private
+  
+  def service_attrs
+    params.require(:articles).permit(:name)
+  end
+
+end
