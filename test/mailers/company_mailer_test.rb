c442995d07085d6e92e868f4f8eb564192157b4d
mishkigami <mihail.kodolov@gmail.com>
2020-04-28 17:27:49 +0400

    add test mail, translate

--- test/mailers/company_mailer_test.rb
+++ test/mailers/company_mailer_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class CompanyMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ test 'Welcome email company' do
+   company = create :company
+
+   email = CompanyMailer.welcome_email(company)
+   assert_emails 1 do
+     email.deliver_now
+   end
+   assert_equal [company.email],email.to
+   assert_equal ['from@example.com'], email.from
+  end
 end
--- test/mailers/previews/company_mailer_preview.rb
+++ test/mailers/previews/company_mailer_preview.rb
@@ -1,4 +1,7 @@
-# Preview all emails at http://localhost:3000/rails/mailers/company_mailer
 class CompanyMailerPreview < ActionMailer::Preview
+    def welcome_email
+        company = FactoryBot.create :company
+        CompanyMailer.welcome_email(company)
+    end
 
 end
