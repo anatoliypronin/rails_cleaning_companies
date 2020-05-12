76471c2257697d8bf4a24335b07bae17d0766b65
mishkigami <mihail.kodolov@gmail.com>
2020-05-12 23:35:07 +0400

    add permit for order_params

--- app/controllers/web/company/orders_controller.rb
+++ app/controllers/web/company/orders_controller.rb
@@ -6,4 +6,10 @@ class Web::Company::OrdersController < Web::Company::ApplicationController
     def show
         @orders = Order.where(company_id: current_company.id)
     end
+
+    private 
+
+    def order_params
+        params.require(:order).permit(:data_start, :data_end, :review, client_id: [], company_id: [], service_id:[])
+    end
 end
