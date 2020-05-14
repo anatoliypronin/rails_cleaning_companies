d978b19898754fb82369d38cc0d94e792d7f02a1
Artem Trepalin <atrepalin@simtechdev.com>
2020-05-14 23:25:31 +0300

    45_fix_model_relationship_with_order : fix tests after refactor relations

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -3,21 +3,11 @@ require 'test_helper'
 class OrderTest < ActiveSupport::TestCase
   test 'Should create order' do
     order = create :order
-    assert order.service
-    assert order.client
-    assert order.company
+    assert order.service_price
   end
 
   test "Shouldn't create, date_start is not valid" do
     order = build :order, date_start: 'TEST'
-    order.save
-
-    created_order = Order.last
-    assert_not created_order
-  end
-
-  test "Shouldn't create, date_start is after date_end" do
-    order = build :order, date_start: '2091-04-09 15:00:25'
     order.save
 
     created_order = Order.last
