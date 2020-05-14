9efd0b97e716a5ab045cf854e0e9606a4c1521df
Artem Trepalin <atrepalin@simtechdev.com>
2020-05-14 23:41:32 +0300

    45_fix_model_relationship_with_order : remove line with index removes

--- db/migrate/20200514190132_fix_order_relation.rb
+++ db/migrate/20200514190132_fix_order_relation.rb
@@ -6,8 +6,6 @@ class FixOrderRelation < ActiveRecord::Migration[6.0]
     remove_column :orders, :service_id
     add_reference :orders, :service_price, foreign_key: true
     change_column :orders, :date_end, :datetime, :null => true
-    remove_index :orders, name: 'index_orders_on_company_id'
-    remove_index :orders, name: 'index_orders_on_service_id'
     add_column :orders, :price, :int
   end
 end
