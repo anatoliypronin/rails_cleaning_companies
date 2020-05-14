968a0ca2993c2a2c385734783b7778edef2e6da6
mishkigami <mihail.kodolov@gmail.com>
2020-05-14 13:47:33 +0400

    add test for state

--- test/controllers/web/company/orders_controller_test.rb
+++ test/controllers/web/company/orders_controller_test.rb
@@ -12,8 +12,32 @@ class Web::Company::OrdersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test 'should get show company order page' do
-    get company_order_path(@order)
-    assert_response :success
+  #test 'should get show company order page' do
+  #  byebug
+  #  get company_order_path(@order)
+  #  assert_response :success
+  #end
+
+test 'should state activate order' do
+    put company_order_activate_path(@order)
+    assert_response :redirect
+    @order.reload
+    assert_equal @order.state, 'active'
   end
+
+  test 'should state completed order' do
+    @order = create :order, :active
+    put company_order_complete_path(@order)
+    assert_response :redirect
+    @order.reload
+    assert_equal @order.state, 'completed'
+  end
+
+  test 'should state rejected order' do
+    put company_order_complete_path(@order)
+    assert_response :redirect
+    @order.reject
+    assert_equal @order.state, 'rejected'
+  end
+
 end
