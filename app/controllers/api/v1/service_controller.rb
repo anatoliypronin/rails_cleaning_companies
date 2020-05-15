8460bd8137765380e9fb07fb53baf7f7560680c9
Artem Trepalin <atrepalin@simtechdev.com>
2020-05-15 17:49:13 +0300

    44_make_order_processing : remove old service api controller

--- app/controllers/api/v1/service_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::ServiceController < Api::V1::ApplicationController
-  def show
-    companies = Company.select('companies.id, companies.name, service_prices.price').joins(:service_prices).where({ 'service_prices.service_id' => params[:id] })
-    render json: companies
-  end
-end
