b134cd2e2dd6c9299df6e58622115d4ad408f8fd
Artem Trepalin <atrepalin@simtechdev.com>
2020-05-15 17:50:54 +0300

    44_make_order_processing : add js script for service_price select2

--- app/assets/javascripts/order_generating.js
+++ app/assets/javascripts/order_generating.js
@@ -1,65 +1,65 @@
 $(document).ready(() => {
-  var company = $('#order_company_id');
-  var service = $('#order_service_id');
-  var company_label = $("label[for='order_company_id']");
-  company.hide();
-  company_label.hide();
-  service.change(() => {
-    $('#order_service_id option:selected').each(() => {
-      company_label.show();
-      company.select2({
-        ajax: {
-          url: "http://localhost:8080/api/v1/service/" + service.val(),
-          dataType: 'json',
-          delay: 250,
-          quietMillis: 100,
-          processResults: function(data)
-          {
-            return {
-              results: $.map(data, function(obj) {
-                  return {
-                    id: obj.id,
-                    name: obj.name,
-                    price: obj.price
-                  };
-                })
+  var service_price = $('#order_service_price_id');
+  service_price.select2({
+    ajax: {
+      url: "http://localhost:8080/api/v1/service_price",
+      dataType: 'json',
+      delay: 250,
+      quietMillis: 100,
+      data: function (params) {
+        return {
+          term: params.term
+        };
+      },
+      processResults: function(data)
+      {
+        return {
+          results: $.map(data, function(obj) {
+              return {
+                id: obj.id,
+                service_name: obj.service_name,
+                company_name: obj.company_name,
+                city_name: obj.city_name,
+                price: obj.price
               };
-            },
-          cache: true
+            })
+          };
         },
-        allowClear: true,
-        placeholder: 'Search for companies',
-        templateResult: formatCompany,
-        templateSelection: formatCompanySelection
-      });
-      $('.select2-container').width('100%');
-    });
-    if (service.val() == '') {
-      $('.select2-container').hide();
-      company.hide();
-      company_label.hide();
-    }
+      cache: true
+    },
+    allowClear: true,
+    placeholder: 'Поиск по сервисам',
+    templateResult: formatService,
+    templateSelection: formatServiceSelection
   });
+  $('.select2-container').width('100%');
 })
 
-function formatCompany (company) {
-  if (company.loading) {
-    return company.text;
+function formatService (service) {
+  if (service.loading) {
+    return service.text;
   }
 
   var $container = $(
-    "<div class='select2-result-company__name clearfix'>" +
-      "<div class='select2-result-company__name'></div>" +
-      "<div class='select2-result-company__price'></div>" +
+    "<div class='select2-result-service__service_name clearfix'>" +
+      "<div class='select2-result-service__service_name'></div>" +
+      "<div class='select2-result-service__company_name'></div>" +
+      "<div class='select2-result-service__city_name'></div>" +
+      "<div class='select2-result-service__price'></div>" +
     "</div>"
   );
 
-  $container.find(".select2-result-company__name").text(company.name);
-  $container.find(".select2-result-company__price").text(company.price + ' руб.');
+  $container.find(".select2-result-service__service_name").text('Сервис: ' + service.service_name);
+  $container.find(".select2-result-service__company_name").text('Компания: ' + service.company_name);
+  $container.find(".select2-result-service__city_name").text('Город: ' + service.city_name);
+  $container.find(".select2-result-service__price").text('Цена: ' + service.price + ' руб.');
 
   return $container;
 }
 
- function formatCompanySelection (company) {
-  return company.name;
+ function formatServiceSelection (service) {
+  if (service.service_name && service.price) {
+    return service.service_name + ' ' + service.price + 'руб.';
+  }
+  return 'Поиск по сервисам'
  }
