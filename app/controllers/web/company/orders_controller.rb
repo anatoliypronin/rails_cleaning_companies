bdaf8fa96205cc8ce2834bb3b2bbfc069e086bb1
mishkigami <mihail.kodolov@gmail.com>
2020-05-16 16:39:00 +0400

    fix action index

--- app/controllers/web/company/orders_controller.rb
+++ app/controllers/web/company/orders_controller.rb
@@ -1,34 +1,36 @@
 class Web::Company::OrdersController < Web::Company::ApplicationController
-    def index
-        @orders = Order.where(service_price_id: current_company.id)
-    end
-    def show
-        @order = current_company.orders.find(params[:id])
-    end
+  def index
+    @orders = current_company.orders
+  end
 
-    def activate
-        order = Order.find(params[:order_id])
-        order.activate
-        redirect_to action: :index
-    end
+  def show
+    @order = current_company.orders.find(params[:id])
+  end
 
-    def complete
-        order = Order.find(params[:order_id])
-        order[:date_end] = DateTime.now 
-        order.complete
-        
-        redirect_to action: :index
-    end
+  def activate
+    order = Order.find(params[:order_id])
+    order.activate
+    redirect_to action: :index
+  end
 
-    def reject
-        order = Order.find(params[:order_id])
-        order.reject
-        redirect_to action: :index
-    end
+  def complete
+    order = Order.find(params[:order_id])
+    order[:date_end] = DateTime.now
+    order.complete
 
-    private 
+    redirect_to action: :index
+  end
 
-    def order_params
-        params.require(:order).permit(:data_start, :data_end, :review, :client_id, :service_price)
-    end
+  def reject
+    order = Order.find(params[:order_id])
+    order[:date_end] = DateTime.now
+    order.reject
+    redirect_to action: :index
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:data_start, :data_end, :review, :client_id, :service_price)
+  end
 end
