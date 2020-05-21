a9d5b059b06a8b487a6d6170617d97a6db757ab9
MishkiGami <mihail.kodolov@gmail.com>
2020-05-21 21:09:26 +0000

    add test for policy editor

--- test/controllers/web/admin/service_prices_controller_test.rb
+++ test/controllers/web/admin/service_prices_controller_test.rb
@@ -68,4 +68,79 @@ class Web::Admin::ServicePriceControllerTest < ActionDispatch::IntegrationTest
     @service_price.reload
     assert_equal @service_price.state, 'active'
   end
+
+  test 'should not get show service_price page for editor' do
+    sign_out_as_admin
+    editor = create :admin, :editor
+    sign_in_as_admin(editor)
+    get admin_service_price_path(@service_price)
+    assert_redirected_to admin_root_path
+  end
+
+  test 'should not get edit service_price page for editor' do
+    sign_out_as_admin
+    editor = create :admin, :editor
+    sign_in_as_admin(editor)
+    get edit_admin_service_price_path(@service_price)
+    assert_redirected_to admin_root_path
+  end
+
+  test 'should not get index service_price page for editor' do
+    sign_out_as_admin
+    editor = create :admin, :editor
+    sign_in_as_admin(editor)
+    get admin_service_prices_path
+    assert_redirected_to admin_root_path
+  end
+
+  test 'should not create service_price by editor' do
+    @city = create :city
+    @company = create :company
+    @service = create :service
+    service_prices_attrs = attributes_for :service_price
+    service_prices_attrs[:city_id] = @city.id
+    service_prices_attrs[:company_id] = @company.id
+    service_prices_attrs[:service_id] = @service.id
+    sign_out_as_admin
+    editor = create :admin, :editor
+    sign_in_as_admin(editor)
+    post admin_service_prices_path, params: { service_price: service_prices_attrs }
+    assert_redirected_to admin_root_path
+    service_price = ServicePrice.last
+    assert_not_equal service_price.city, @city
+    assert_not_equal service_price.service, @service
+    assert_not_equal service_price.company, @company
+  end
+
+  test 'should not state deleted service_price by editor' do
+    sign_out_as_admin
+    editor = create :admin, :editor
+    sign_in_as_admin(editor)
+    put admin_service_price_del_path(@service_price)
+    assert_response :redirect
+    @service_price.reload
+    assert_equal @service_price.state, 'active'
+  end
+
+  test 'should not state active service_price by editor' do
+    @service_price = create :service_price, :disabled
+    sign_out_as_admin
+    editor = create :admin, :editor
+    sign_in_as_admin(editor)
+    put admin_service_price_del_path(@service_price)
+    assert_response :redirect
+    @service_price.reload
+    assert_equal @service_price.state, 'disabled'
+  end
+
+  test 'should not state disable service_price by editor' do
+    @service_price = create :service_price, :active
+    sign_out_as_admin
+    editor = create :admin, :editor
+    sign_in_as_admin(editor)
+    put admin_service_price_del_path(@service_price)
+    assert_response :redirect
+    @service_price.reload
+    assert_equal @service_price.state, 'active'
+  end
 end
