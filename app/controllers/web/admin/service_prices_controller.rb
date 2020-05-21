3176221eac063c9dd276cb46ee3ebcffb9cd88fb
MishkiGami <mihail.kodolov@gmail.com>
2020-05-21 20:41:58 +0000

    add policy for service_price

--- app/controllers/web/admin/service_prices_controller.rb
+++ app/controllers/web/admin/service_prices_controller.rb
@@ -1,14 +1,17 @@
 class Web::Admin::ServicePricesController < Web::Admin::ApplicationController
   def index
     @service_prices = ServicePrice.all
+    authorize current_user, policy_class: AdminPolicy
   end
 
   def new
     @service_price = ServicePrice.new
+    authorize current_user, policy_class: AdminPolicy
   end
 
   def create
     @service_price = ServicePrice.new(service_prices_attrs)
+    authorize current_user, policy_class: AdminPolicy
     if @service_price.save
       redirect_to action: :index
     else
@@ -18,14 +21,17 @@ class Web::Admin::ServicePricesController < Web::Admin::ApplicationController
 
   def show
     @service_price = ServicePrice.find(params[:id])
+    authorize current_user, policy_class: ClientPolicy
   end
 
   def edit
     @service_price = ServicePrice.find(params[:id])
+    authorize current_user, policy_class: ClientPolicy
   end
 
   def update
     @service_price = ServicePrice.find(params[:id])
+    authorize current_user, policy_class: ClientPolicy
     if @service_price.update(service_prices_attrs)
       redirect_to action: :index
     else
@@ -35,18 +41,21 @@ class Web::Admin::ServicePricesController < Web::Admin::ApplicationController
 
   def del
     service_price = ServicePrice.find(params[:service_price_id])
+    authorize current_user, policy_class: AdminPolicy
     service_price.del
     redirect_to action: :index
   end
 
   def restore
     service_price = ServicePrice.find(params[:service_price_id])
+    authorize current_user, policy_class: AdminPolicy
     service_price.activate
     redirect_to action: :index
   end
 
   def disable
     service_price = ServicePrice.find(params[:service_price_id])
+    authorize current_user, policy_class: AdminPolicy
     service_price.disable
     redirect_to action: :index
   end
