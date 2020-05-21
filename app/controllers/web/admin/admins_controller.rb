420ba048fa65b285ca8445e94b9857a8094bad8b
MishkiGami <mihail.kodolov@gmail.com>
2020-05-21 20:16:49 +0000

    add policy for admin

--- app/controllers/web/admin/admins_controller.rb
+++ app/controllers/web/admin/admins_controller.rb
@@ -1,14 +1,17 @@
 class Web::Admin::AdminsController < Web::Admin::ApplicationController
   def index
     @admins = Admin.all
+    authorize current_user
   end
 
   def new
     @admin = Admin.new
+    authorize current_user
   end
 
   def create
     @admin = Admin.new(admins_attrs)
+    authorize current_user
     if @admin.save
       redirect_to action: :index
     else
@@ -18,14 +21,17 @@ class Web::Admin::AdminsController < Web::Admin::ApplicationController
 
   def show
     @admin = Admin.find(params[:id])
+    authorize @admin
   end
 
   def edit
     @admin = Admin.find(params[:id])
+    authorize @admin
   end
 
   def update
     @admin = Admin.find(params[:id])
+    authorize @admin
     if @admin.update(admins_attrs)
       redirect_to action: :index
     else
@@ -35,12 +41,14 @@ class Web::Admin::AdminsController < Web::Admin::ApplicationController
 
   def del
     admin = Admin.find(params[:admin_id])
+    authorize admin
     admin.del
     redirect_to action: :index
   end
 
   def restore
     admin = Admin.find(params[:admin_id])
+    authorize admin
     admin.activate
     redirect_to action: :index
   end
