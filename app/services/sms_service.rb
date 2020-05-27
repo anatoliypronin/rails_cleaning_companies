9fa4a592acc2da5a6dd358a9e2e8947b408df72e
mishkigami <mihail.kodolov@gmail.com>
2020-05-27 22:40:13 +0400

    add service for sms code for test

---
+++ app/services/sms_service.rb
@@ -0,0 +1,13 @@
+module SmsService
+  TEST_VERIFICATION_CODE = '1234'.freeze
+  class << self
+    def send_sms_code(phone_number)
+      return TEST_VERIFICATION_CODE if Rails.env.test?
+
+      code = rand(1000..9999).to_s
+      message = "Проверочный код: #{code}"
+      Gateways::SmsGateway::Company.send_sms_message(phone_number, message)
+      code
+    end
+  end
+end
