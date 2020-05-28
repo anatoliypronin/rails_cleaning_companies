887dce040ced0825caafa53c40816cd7167eab4d
Artem Trepalin <atrepalin@simtechdev.com>
2020-05-28 13:45:05 +0300

    48_create_client_mobile_auth_backend : add sms_service module

---
+++ app/services/sms_service.rb
@@ -0,0 +1,15 @@
+module SmsService
+  TEST_VERIFICATION_CODE = '1234'
+  class << self
+    def send_sms_code(phone_number)
+      return TEST_VERIFICATION_CODE if Rails.env.test?
+      code = rand(1000..9999).to_s
+
+      message = I18n.t('.varification_code', :code => code)
+
+      Gateways::SmsGateway::Client.send_sms_message(phone_number, message)
+
+      code
+    end
+  end
+end
