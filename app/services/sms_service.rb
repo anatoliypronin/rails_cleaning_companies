c189662847cc5f07b7167ecd152149ff89b1564e
Artem Trepalin <atrepalin@simtechdev.com>
2020-05-28 15:38:21 +0300

    48_create_client_mobile_auth_backend : fix grammarly mistake in locale

--- app/services/sms_service.rb
+++ app/services/sms_service.rb
@@ -6,7 +6,7 @@ module SmsService
 
       code = rand(1000..9999).to_s
 
-      message = I18n.t('.varification_code', code: code)
+      message = I18n.t('.verification_code', code: code)
 
       Gateways::SmsGateway::Client.send_sms_message(phone_number, message)
 
