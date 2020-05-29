091e758291c0d2f082a9b178c8492e3e13af64ce
mishkigami <mihail.kodolov@gmail.com>
2020-05-29 16:38:07 +0400

    change path curl sms sent

--- lib/gateways/sms_gateway/client.rb
+++ lib/gateways/sms_gateway/client.rb
@@ -4,7 +4,7 @@ module Gateways::SmsGateway::Client
   module_function
 
   def send_sms_message(phone_number, message)
-    http = Curl::Easy.new("https://medvejatka%40yandex.ru:GM6IxNtjKOpPwipx2amJKbgkGnCv@gate.smsaero.ru/v2/sms/send?number=
+    http = Curl::Easy.new("https://email:api_keyGM6IxNtjKOpPwipx2amJKbgkGnCv@gate.smsaero.ru/v2/sms/send?number=
                           #{phone_number}&text=#{message}&sign=SMS Aero&channel=DIRECT")
     http.perform
   end
