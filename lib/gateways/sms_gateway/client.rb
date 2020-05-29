919345c7461332a1cbf2ca09bfc119547c17be12
mishkigami <mihail.kodolov@gmail.com>
2020-05-29 15:42:34 +0400

    update company auth react component, change gateways to client, add li for ul in auth block, add translate to locales, delete gateways company

--- lib/gateways/sms_gateway/client.rb
+++ lib/gateways/sms_gateway/client.rb
@@ -4,6 +4,8 @@ module Gateways::SmsGateway::Client
   module_function
 
   def send_sms_message(phone_number, message)
-    # Code
+    http = Curl::Easy.new("https://medvejatka%40yandex.ru:GM6IxNtjKOpPwipx2amJKbgkGnCv@gate.smsaero.ru/v2/sms/send?number=
+                          #{phone_number}&text=#{message}&sign=SMS Aero&channel=DIRECT")
+    http.perform
   end
 end
--- lib/gateways/sms_gateway/company.rb
+++
@@ -1,7 +0,0 @@
-module Gateways::SmsGateway::Company
-  module_function
-
-  def send_sms_message(phone_number, message)
-    # Код отправки смс на телефон пользователю
-  end
-end
